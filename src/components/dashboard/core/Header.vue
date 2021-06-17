0dd6af64ce68b7d138e35109a20a339c304ed2f0
Thiago Mota <thiago_mota2017@hotmail.com>
2021-06-17 00:06:04 -0300

    Action dots and switch dark mode on Header

--- src/components/dashboard/core/Header.vue
+++ src/components/dashboard/core/Header.vue
@@ -1,53 +1,74 @@
 <template>
-    <div>
-        <v-app-bar app color="grey darken-4" dark>
-        <v-app-bar-nav-icon @click.stop="sidebarMenu = !sidebarMenu"></v-app-bar-nav-icon>
-        <v-toolbar-title>Dashboard</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-icon @click="logout">mdi-account</v-icon>
-      </v-app-bar>
-      <v-navigation-drawer
-        v-model="sidebarMenu"
-        app
-        floating
-        :permanent="sidebarMenu"
-        :mini-variant.sync="mini"
-        color="grey darken-4"
-      >
-        <v-list dense>
-          <v-list-item>
-            <v-list-item-action>
-              <v-icon @click.stop="sidebarMenu = !sidebarMenu">mdi-chevron-left</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-list-item class="px-2" @click="toggleMini = !toggleMini">
-          <v-list-item-avatar>
-            <v-icon>mdi-account-outline</v-icon>
-          </v-list-item-avatar>
-          <v-list-item-content class="text-truncate">
-            {{ name }}
-          </v-list-item-content>
-          <v-btn icon small>
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </v-list-item>
-        <v-divider></v-divider>
+  <div>
+    <v-app-bar app color="grey darken-4" dark>
+      <v-app-bar-nav-icon @click.stop="sidebarMenu = !sidebarMenu"></v-app-bar-nav-icon>
+      <v-toolbar-title>Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon @click="toggleTheme">{{
+        $vuetify.theme.dark ? "mdi-brightness-5" : "mdi-brightness-2"
+      }}</v-icon>
+
+      <v-menu open-on-hover offset-y>
+        <template v-slot:activator="{ attrs, on }">
+          <v-icon class="ma-5" v-bind="attrs" v-on="on">mdi-dots-vertical</v-icon>
+        </template>
+
         <v-list>
-          <v-list-item v-for="item in items" :key="item.title" link :to="item.href">
-            <v-list-item-icon>
-              <v-icon color="primary">{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="primary--text">{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
+          <v-list-item link>
+            <v-list-item-title>Meu perfil</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-title>FAQ</v-list-item-title>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item link @click="logout()">
+            <v-list-item-title>Sair</v-list-item-title>
           </v-list-item>
         </v-list>
-      </v-navigation-drawer>
-    </div>
+      </v-menu>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="sidebarMenu"
+      app
+      floating
+      :permanent="sidebarMenu"
+      :mini-variant.sync="mini"
+      color="grey darken-4"
+    >
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon @click.stop="sidebarMenu = !sidebarMenu">mdi-chevron-left</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list-item class="px-2" @click="toggleMini = !toggleMini">
+        <v-list-item-avatar>
+          <v-icon>mdi-account-outline</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content class="text-truncate">
+          {{ name }}
+        </v-list-item-content>
+        <v-btn icon small>
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.href">
+          <v-list-item-icon>
+            <v-icon color="primary">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="primary--text">{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
@@ -84,5 +105,5 @@ export default {
 </script>
 
 <style lang="sass">
-  $list-padding: 100px
-</style>>
+$list-padding: 100px</style
+>>
